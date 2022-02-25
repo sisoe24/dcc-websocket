c86d7dc1eb6283409af672d37dba3ff0076a6594
virgilsisoe <virgilsisoe@gmail.com>
2022-02-24 20:06:04 -0500

    check if can add stubs to web

--- src/web/extension.ts
+++ src/web/extension.ts
@@ -1,7 +1,15 @@
 import * as vscode from "vscode";
 import { sendMessage } from "./socket";
+import { addStubsPath, correctAnalysisPath } from "./stubs";
 
 export function activate(context: vscode.ExtensionContext) {
+    correctAnalysisPath();
+
+    context.subscriptions.push(
+        vscode.commands.registerCommand("dcc-websocket.addPythonStubs", () => {
+            addStubsPath();
+        })
+    );
     context.subscriptions.push(
         vscode.commands.registerCommand("dcc-websocket.sendCode", () => {
             sendMessage();
